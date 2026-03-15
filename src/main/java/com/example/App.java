a73c437f79e77570ef57bc47256efd1932162300
Hemanth-bs <hemanthbyrareddy@gmail.com>
2026-03-15 11:57:13 +0530

    fist-commit

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,18 +2,38 @@ package com.example;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import java.time.Duration;
 
 public class App {
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws InterruptedException {
 
         WebDriver driver = new ChromeDriver();
 
-        driver.get("https://www.saucedemo.com/");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        driver.get("https://automationexercise.com/products");
+
         driver.manage().window().maximize();
 
-        driver.findElement(By.id("user-name")).sendKeys("standard_user");
-        driver.findElement(By.id("password")).sendKeys("secret_sauce");
-        driver.findElement(By.id("login-button")).click();
+        // Hover over product
+        WebElement product = driver.findElement(By.xpath("(//div[@class='product-image-wrapper'])[4]"));
+
+        Actions actions = new Actions(driver);
+        actions.moveToElement(product).perform();
+
+        // Hover and click Add to Cart
+        WebElement addToCart = driver.findElement(By.xpath("(//a[@data-product-id='4'])[1]"));
+        actions.moveToElement(addToCart).click().perform();
+
+        // Click Continue Shopping
+        driver.findElement(By.xpath("//button[text()='Continue Shopping']")).click();
+
+        Thread.sleep(2000);
+
+        driver.quit();
     }
 }
